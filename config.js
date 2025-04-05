3228de77550087dac3201d05727042dad5864c0f
CodingWithRick <nitubasukundu@gmail.com>
2025-04-05 15:20:49 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,231 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: 'Rick',
+	imageBackground: false,
+	openInNewTab: false,
+	twelveHourFormat: true,
+	timeZone: 'Asia/Calcutta',
+
+	// Greetings
+	greetingMorning: 'Good morning!',
+	greetingAfternoon: 'Good afternoon,',
+	greetingEvening: 'Good evening,',
+	greetingNight: 'Go to Sleep!',
+
+	// Layout
+	bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
+
+	// Weather
+	weatherKey: '26dba7650300b5dda9df64ac45f24579', // Write here your API Key
+	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: 'C', // 'F', 'C'
+	language: 'en', // More languages in https://openweathermap.org/current#multi
+
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '22.572645',
+	defaultLongitude: '88.363892',
+
+	// Autochange
+	autoChangeTheme: false,
+
+	// Autochange by OS
+	changeThemeByOS: true,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Github',
+			icon: 'github',
+			link: 'https://github.com/',
+		},
+		{
+			id: '2',
+			name: 'Mail',
+			icon: 'mail',
+			link: 'https://mail.protonmail.com/',
+		},
+		{
+			id: '3',
+			name: 'Todoist',
+			icon: 'trello',
+			link: 'https://todoist.com',
+		},
+		{
+			id: '4',
+			name: 'Calendar',
+			icon: 'calendar',
+			link: 'https://calendar.google.com/calendar/r',
+		},
+		{
+			id: '5',
+			name: 'Reddit',
+			icon: 'glasses',
+			link: 'https://reddit.com',
+		},
+		{
+			id: '6',
+			name: 'Odysee',
+			icon: 'youtube',
+			link: 'https://odysee.com/',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'star',
+			id: '1',
+			links: [
+				{
+					name: 'Github',
+					link: 'https://github.com/',
+				},
+				{
+					name: 'Mail',
+					link: 'https://mail.google.com/mail/u/0/#inbox',
+				},
+				{
+					name: 'ChatGPT',
+					link: 'https://chat.openai.com/',
+				},
+				{
+					name: 'MonkeyType',
+					link: 'https://monkeytype.com/',
+				},
+			],
+		},
+		{
+			icon: 'tv',
+			id: '2',
+			links: [
+				{
+					name: 'YouTube',
+					link: 'https://www.youtube.com',
+				},
+				{
+					name: 'h!anime',
+					link: 'https://hianime.to/home',
+				},
+				{
+					name: 'Twitch',
+					link: 'https://www.twitch.tv',
+				},
+				{
+					name: 'Lucifer Donghua',
+					link: 'https://luciferdonghua.in/',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'square-user-round',
+			id: '1',
+			links: [
+                {
+					name: 'Physics Wallah',
+					link: 'https://pw.live',
+				},
+                {
+					name: 'Whatsapp',
+					link: 'https://web.whatsapp.com',
+				},
+				{
+					name: 'r/Unix',
+					link: 'https://www.reddit.com/r/unixporn',
+				},
+                {
+                    name: 'IP-Connect',
+                    link: 'http://10.254.254.63/0/up/',
+                },
+			],
+		},
+		{
+			icon: 'binary',
+			id: '2',
+			links: [
+				{
+					name: 'Programming',
+					link: 'https://www.reddit.com/r/programming/',
+				},
+				{
+					name: 'Javascript',
+					link: 'https://www.reddit.com/r/javascript/',
+				},
+				{
+					name: 'CSS',
+					link: 'https://www.reddit.com/r/css/',
+				},
+				{
+					name: 'Python',
+					link: 'https://www.reddit.com/r/Python/',
+				},
+			],
+		},
+	],
+};
